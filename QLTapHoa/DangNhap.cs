cc3955e56cadadd924eb4cda1d77e5d1a5516d59
HSondev74 <111474864+HSondev74@users.noreply.github.com>
2023-10-17 22:21:14 +0700

    tuyen commit

--- QLTapHoa/DangNhap.cs
+++ QLTapHoa/DangNhap.cs
@@ -7,14 +7,121 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ComponentFactory.Krypton.Toolkit;
 
 namespace QLTapHoa
 {
-    public partial class DangNhap : Form
+    public partial class fr_DangNhap : KryptonForm
     {
-        public DangNhap()
+        public fr_DangNhap()
         {
             InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt_User_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fr_DangNhap_Load(object sender, EventArgs e)
+        {
+            
+            string sql = "select * from Quyen";
+            DataTable dt = ketnoi.truyvan(sql);
+            cmb_Quyen.DataSource = dt;
+            cmb_Quyen.DisplayMember = "nguoidung";
+            cmb_Quyen.ValueMember = "Type";
+
+            txt_User.Text = Properties.Settings.Default.user;
+            txt_pass.Text = Properties.Settings.Default.pass;
+
+            if(txt_User.Text != "")
+            {
+                chb_remember.Checked = true;
+            }
+        }
+
+        private void btn_DangNhap_Click(object sender, EventArgs e)
+        {
+            string user, pass;
+            user = txt_User.Text;
+            pass = txt_pass.Text;
+            string typeUser = cmb_Quyen.SelectedValue.ToString();
+            MessageBox.Show(typeUser);
+            string sql = $"select * from Account where type='{typeUser}' ";
+            DataTable dt = ketnoi.truyvan(sql);
+            bool kt = false;
+            foreach(DataRow dr in dt.Rows)
+            {
+                if ((dr[1].ToString().Trim()== user) && (dr[2].ToString().Trim()== pass)) { kt = true; break; } 
+            }
+            if(kt == true)
+            {
+                MessageBox.Show("Ban nhap Thanh cong", "Thong Bao", MessageBoxButtons.OKCancel);
+                Form1 form1 = new Form1();
+                form1.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Ban nhap khong dung", "Thong Bao", MessageBoxButtons.OKCancel);
+                txt_User.Focus();
+            }
+        }
+
+        private void chb_remember_CheckedChanged(object sender, EventArgs e)
+        {
+            if(chb_remember.Checked == true)
+            {
+                Properties.Settings.Default.user = txt_User.Text;
+                Properties.Settings.Default.pass = txt_pass.Text;
+                Properties.Settings.Default.Save();
+            }
+            else
+            {
+                Properties.Settings.Default.Reset();
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        private void btn_openPass_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        private void btn_ClosePass_Paint(object sender, PaintEventArgs e)
+        {
+           
+        }
+
+        private void btn_ClosePass_Click(object sender, EventArgs e)
+        {
+            if (txt_pass.PasswordChar == '\0')
+            {
+                btn_openPass.BringToFront();
+                txt_pass.PasswordChar = '*';
+            }
+        }
+
+        private void btn_openPass_Click(object sender, EventArgs e)
+        {
+            if (txt_pass.PasswordChar == '*')
+            {
+                btn_ClosePass.BringToFront();
+                txt_pass.PasswordChar = '\0';
+            }
+        }
+
+        private void kryptonLinkLabel1_LinkClicked(object sender, EventArgs e)
+        {
+            QuenMK quenMK = new QuenMK();
+            quenMK.Show();
+            this.Hide();
         }
     }
 }
---
+++ QLTapHoa/QuenMK.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace QLTapHoa
+{
+    partial class QuenMK
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.kryptonTextBox1 = new ComponentFactory.Krypton.Toolkit.KryptonTextBox();
+            this.kryptonTextBox2 = new ComponentFactory.Krypton.Toolkit.KryptonTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.kryptonLinkLabel1 = new ComponentFactory.Krypton.Toolkit.KryptonLinkLabel();
+            this.SuspendLayout();
+            // 
+            // kryptonTextBox1
+            // 
+            this.kryptonTextBox1.Location = new System.Drawing.Point(305, 125);
+            this.kryptonTextBox1.Name = "kryptonTextBox1";
+            this.kryptonTextBox1.Size = new System.Drawing.Size(211, 31);
+            this.kryptonTextBox1.StateCommon.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.kryptonTextBox1.StateCommon.Border.Rounding = 5;
+            this.kryptonTextBox1.TabIndex = 0;
+            // 
+            // kryptonTextBox2
+            // 
+            this.kryptonTextBox2.Location = new System.Drawing.Point(305, 194);
+            this.kryptonTextBox2.Name = "kryptonTextBox2";
+            this.kryptonTextBox2.Size = new System.Drawing.Size(211, 31);
+            this.kryptonTextBox2.StateCommon.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.kryptonTextBox2.StateCommon.Border.Rounding = 5;
+            this.kryptonTextBox2.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.Location = new System.Drawing.Point(233, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(401, 52);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "QUEN MAT KHAU";
+            // 
+            // kryptonLinkLabel1
+            // 
+            this.kryptonLinkLabel1.Location = new System.Drawing.Point(329, 268);
+            this.kryptonLinkLabel1.Name = "kryptonLinkLabel1";
+            this.kryptonLinkLabel1.Size = new System.Drawing.Size(96, 24);
+            this.kryptonLinkLabel1.TabIndex = 2;
+            this.kryptonLinkLabel1.Values.Text = "Tro ve Login";
+            this.kryptonLinkLabel1.LinkClicked += new System.EventHandler(this.kryptonLinkLabel1_LinkClicked);
+            // 
+            // QuenMK
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.kryptonLinkLabel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.kryptonTextBox2);
+            this.Controls.Add(this.kryptonTextBox1);
+            this.Name = "QuenMK";
+            this.Text = "QuenMK";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ComponentFactory.Krypton.Toolkit.KryptonTextBox kryptonTextBox1;
+        private ComponentFactory.Krypton.Toolkit.KryptonTextBox kryptonTextBox2;
+        private System.Windows.Forms.Label label1;
+        private ComponentFactory.Krypton.Toolkit.KryptonLinkLabel kryptonLinkLabel1;
+    }
+}
